cbe4d4c8fd3cd7b3455c8c2d5312ab0b0550ac5e
alex <alex@421fdda0-16d4-4d65-83a8-de151944c2bf>
2008-02-07 22:49:53 +0000

    #26: группы для класса параметров

--- app/models/Params.php
+++ app/models/Params.php
@@ -1,25 +1,56 @@
 <?php
 	class Params extends CBaseModel{
 		
-		public function getParam($name){
-			$this->resetSql();
-			$this->where('item_name="'.$name.'"');
-			$this->cols('value');
-			$rez = $this->getOne();
-			if(isset($rez['value'])) return $rez['value'];
-			else return null;			
-		}
-		
-		function getParamsGroup($name){
-			$this->resetSql();
-			$this->where('group_name="'.$name.'"');
-			$this->cols('item_name, value');			
-			$sqlRez = $this->getAll();
-			$rez = array();
-			foreach($sqlRez as $item){
-				$rez[$item['item_name']] = $item['value'];
+		public function getParam($group_name, $param_name){
+			if ($group_name !== null){
+				// TODO:: need caching
+				$this->resetSql();
+				$this-> join('params_group', 'params_group.id = params.params_group_id AND LOWER(params_group.label)=LOWER("'.$this -> escape($group_name).'")', "INNER");
+				$this->where(' LOWER(params.name) = LOWER("'.$this -> escape($param_name).'")');
+				$this->cols('params.value as value, params.php_type as php_type');
+				$rez = $this->getOne();
+			} else {
+				$this->resetSql();
+				$this->where(' LOWER(name) = LOWER("'.$this -> escape($param_name).'")');
+				$this->where(' CAST(params_group_id AS UNSIGNED) = 0');
+				$this->cols('*');
+				$rez = $this->getOne();
 			}
-			return $rez;
+			
+			if (count($rez) === 0){
+				return null;
+			}
+			$default_type = "string";
+			$param_type = trim(strtolower($rez['php_type']));
+			$value = $rez['value'];
+			$ret = $value;
+			switch($param_type){
+				case "string":
+						$ret = $value;
+						break;
+				case "integer":
+						$ret = (int)$value;
+						break;
+				case "float":
+						$ret = (float)$value;
+						break;
+				case "array":
+						$tmp = unserialize($value);
+						if (is_array($value)){
+							$ret = $tmp;
+						} else {
+							$ret = array();
+						}
+						break;
+				case "boolean":
+						$ret = (bool)$value;
+						break;
+				default:
+						$ret = $value;
+						break;
+					
+			}
+			return $ret;
 		}
 	}
 ?>
--- core/managers/CParams.php
+++ core/managers/CParams.php
@@ -2,15 +2,20 @@
 	class CParams extends CBaseManager {
 		
 		public function __construct(){
+			
+		}
+		
+		public function getParam($param_group, $name){
 			$this->setModel('Params');
+			return $this->model->getParam($param_group, $name);
 		}
 		
-		public function getParam($name){
-			return $this->model->getParam($name);
-		}
-		
-		public function getParamsGroup($name){
-			return $this->model->getParamsGroup($name);
+		public function getParamsOfGroup($group_name){
+			$this->setModel('Params');
+			$this->resetSql();
+			$this-> join('params_group', 'params_group.id = params.params_group_id AND LOWER(params_group.label)=LOWER("'.$this -> escape($group_name).'")', "INNER");
+			$this->cols('params.name as name, params.value as value, params.php_type as php_type');
+			return $this->getAll();
 		}
 	}
 ?>
