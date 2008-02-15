ba1db43b96ca99d1bc44e41e7b32a4d056ac43f3
victor <victor@421fdda0-16d4-4d65-83a8-de151944c2bf>
2008-02-15 18:31:44 +0000

    текущее меню

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -3,41 +3,36 @@
  * Контролер администрирования системы
  */
 	class AdminController extends CBaseController{
-		
+
 		function __construct($View=null, $params = array(), $vars = array()){
 			parent::__construct($View, $params, $vars);
 			$this -> view -> setTemplate(VIEWS_PATH.'admin/main.tpl.php');
 		}
-		
+
 		protected function BaseAdminData(){
 			$router = getManager('CRouter');
 			// TODO:: hardcoded menu
 			$this -> view -> main_menu = array(
 												array('link'=>$router -> createUrl('Admin', 'Desktop'), 'name'=>'Рабочий стол'),
-												array('link'=>'#', 'name'=>'Каталог сложных опросов'),
-												array('link'=>'#', 'name'=>'Каталог лент новостей'),
-												array('link'=>'#', 'name'=>'Блоги пользователей'),
-												array('link'=>'#', 'name'=>'Дневники пользователей'),
 												array('link'=>$router -> createUrl('AdminParameter', 'GroupList'), 'name'=>'Параметры системы'),
 												array('link'=>$router -> createUrl('AdminUser', 'List'), 'name'=>'Пользователи'),
 												);
-			
+
 			$session = getManager('CSession');
 			$user = unserialize($session->read('user'));
 		}
-		
-		
+
+
 		public function DesktopAction(){
 			$this -> BaseAdminData();
 			$this -> view -> title = 'Рабочий стол';
 			$this -> view -> content .= $this->view->render(VIEWS_PATH.'admin/desktop.tpl.php');
 			$this -> view -> display();
 		}
-		
-		
-		
+
+
+
 		public function LoginFormAction(){
-			
 			$session = getManager('CSession');
 			$user = unserialize($session->read('user'));
 			if (is_array($user) && isset($user['id']) && ((int)$user['id'] > 0)){
@@ -49,10 +44,10 @@
 			$this -> view -> setTemplate(VIEWS_PATH.'admin/login.tpl.php');
 			$this->view->display();
 		}
-		
-		
+
+
 		public function LoginAction(){
-			
+
 			$session = getManager('CSession');
 			$user = unserialize($session->read('user'));
 			if (is_array($user) && isset($user['id']) && ((int)$user['id'] > 0)){
@@ -63,10 +58,10 @@
 				$router = getManager('CRouter');
 				$router -> redirect($router -> createUrl('Admin', 'LoginForm'));
 			}
-			
+
 			$this -> setModel("Users");
 			$login_result = $this -> model -> login($this -> u_login, $this -> u_pass);
-			
+
 			if ($login_result === false){
 				$this -> view -> login_result = false;
 			} else {
@@ -76,15 +71,15 @@
 				$router = getManager('CRouter');
 				$router -> redirect($router -> createUrl('Admin', 'Desktop'));
 			}
-			
-			
-			
+
+
+
 			$request_user_id = (int)$this -> id;
-			
+
 			$this -> view -> setTemplate(VIEWS_PATH.'admin/login.tpl.php');
 			$this->view->display();
 		}
-		
+
 		public function LogoutAction(){
 			$user = getManager('CUser');
 			$user -> logout();
