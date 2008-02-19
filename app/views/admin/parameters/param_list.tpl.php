ad0ce187b8cf7d105e7e9f6b02c24afc3f08c354
alex <alex@421fdda0-16d4-4d65-83a8-de151944c2bf>
2008-02-19 20:18:09 +0000

    system parameters administration

---
+++ app/views/admin/parameters/param_list.tpl.php
@@ -0,0 +1,64 @@
+<?php include($this -> _include('../header.tpl.php')); ?>
+	<div class="list">
+ 	    <div style="float: left;"><h3><?php if($this -> controller_id>0){ echo $this -> controller_name;} else {echo 'Общие параметры системы';}?></h3><?php if (strlen($this -> controller_description)){echo " (".$this -> controller_description.")";}?></div>
+ 	
+ 	    <table class="list_table">
+ 	        <tr class="head">
+ 	            <td class="first" rowspan="100">&nbsp;</td>
+ 	            <td>
+ 	                N
+	            </td>
+ 	            <td>
+ 	                Название
+ 	            </td>
+ 	            <td>
+ 	                Значение
+ 	            </td>
+ 	            <td>
+ 	                Тип
+ 	            </td>
+ 	            <td>
+ 	                Действие
+ 	            </td>
+ 	            <td class="last" rowspan="100">&nbsp;</td>
+ 	        </tr>
+ 	        <form action="<?php echo $this -> save_action;?>" method="post">
+ 	            <input type="hidden" name="id" value="<?php echo $this -> param_group_id; ?>"/>
+ 	            <input type="hidden" name="controller_id" value="<?php echo $this -> controller_id; ?>"/>
+ 	            <input type="hidden" name="param_group_id" value="<?php echo $this -> param_group_id; ?>"/>
+ 	            
+ 	            <?php foreach($this -> param_list as $item) { ?>
+	                <input type="hidden" name="ids[<?php echo $item['id'];?>]" value="<?php echo $item['id']; ?>"/>
+ 	            <tr>
+ 	                <td>
+ 	                    <?php echo $item['number']; ?>
+ 	                </td>
+ 	                <td>
+ 	                    <input type="text" name="param_name[<?php echo $item['id'];?>]" value="<?php echo $item['name']; ?>"/>
+ 	                </td>
+ 	                <td>
+ 	                    <input type="text" name="param_value[<?php echo $item['id'];?>]" value="<?php echo $item['value']; ?>"/>
+ 	                </td>
+ 	                <td>
+ 	                    <select name="php_type[<?php echo $item['id'];?>]">
+ 	                        <?php foreach($this -> php_types as $key=>$value){ ?>
+ 	                            <option value="<?php echo $key;?>" <?php if ($key==$item['php_type']) echo 'selected'; ?>><?php echo $value;?></option>
+ 	                        <? } ?>
+ 	                    </select>
+ 	                </td>
+ 	                <td>
+ 	                	<?php if ($item['id'] > 0 ) { ?>
+ 	                    	<div class="button bsmall" style="float: left;"><a href="<?php echo $item['delete_link'];?>"><img src="<?php echo $this -> image_url;?>icons/small_del.gif" alt="Удалить"/></a></div>
+ 	                    <?php } ?>
+ 	                </td>
+ 	   
+ 	            </tr>
+ 	            <?php } ?>
+ 	            <tr>
+ 	                <td colspan="4" align="center"><input type="submit" value="Сохранить" /></td>
+ 	            </tr>
+ 	        <form>
+ 	    </table>
+ 	
+	</div>
+<?php include($this -> _include('../footer.tpl.php')); ?>
