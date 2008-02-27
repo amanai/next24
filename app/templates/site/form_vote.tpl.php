44f21ff0f1c570ac271fc6a57b6107aa91afb32e
alex <alex@421fdda0-16d4-4d65-83a8-de151944c2bf>
2008-02-27 10:21:56 +0000

    photo votes from 0 to 10

--- app/templates/site/form_vote.tpl.php
+++ app/templates/site/form_vote.tpl.php
@@ -2,30 +2,30 @@
 <input type="hidden" name="id" value="<?php echo $this->element_id;?>" />
 <table>
 <tr align="center">
-<td>-5</td>
-<td>-4</td>
-<td>-3</td>
-<td>-2</td>
-<td>-1</td>
 <td>0</td>
 <td>1</td>
 <td>2</td>
 <td>3</td>
 <td>4</td>
 <td>5</td>
+<td>6</td>
+<td>7</td>
+<td>8</td>
+<td>9</td>
+<td>10</td>
 </tr>
 <tr align="center">
-<td><input type="radio" name="rate_value" value="-5" /></td>
-<td><input type="radio" name="rate_value" value="-4" /></td>
-<td><input type="radio" name="rate_value" value="-3" /></td>
-<td><input type="radio" name="rate_value" value="-2" /></td>
-<td><input type="radio" name="rate_value" value="-1" /></td>
-<td><input type="radio" name="rate_value" value="0" checked="checked" /></td>
+<td><input type="radio" name="rate_value" value="0" /></td>
 <td><input type="radio" name="rate_value" value="1" /></td>
 <td><input type="radio" name="rate_value" value="2" /></td>
 <td><input type="radio" name="rate_value" value="3" /></td>
 <td><input type="radio" name="rate_value" value="4" /></td>
-<td><input type="radio" name="rate_value" value="5" /></td>
+<td><input type="radio" name="rate_value" value="5" checked="checked" /></td>
+<td><input type="radio" name="rate_value" value="6" /></td>
+<td><input type="radio" name="rate_value" value="7" /></td>
+<td><input type="radio" name="rate_value" value="8" /></td>
+<td><input type="radio" name="rate_value" value="9" /></td>
+<td><input type="radio" name="rate_value" value="10" /></td>
 </tr>
 <tr>
 <td colspan="11" align="center">
