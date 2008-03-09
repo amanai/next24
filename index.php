1e14837e5f2b84394502bbbcc3a8e1e02bd3b2aa
alex <alex@421fdda0-16d4-4d65-83a8-de151944c2bf>
2008-03-09 21:20:03 +0000

    time checking

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+	$t1 = microtime(true);
+	$GLOBALS['query_counter'] = 0;
 	define('USER_UPLOAD_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'users');
 	error_reporting(E_ALL);
 	include 'core' . DIRECTORY_SEPARATOR . 'Project.php';
@@ -8,4 +10,7 @@
 	$app -> init(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'main_config.xml');	
 	$controller = $app -> run();
 	$app -> complete($controller);
+	$t2 = microtime(true);
+	//var_dump(($t2 - $t1));
+	//var_dump($GLOBALS['query_counter']);
 ?>
