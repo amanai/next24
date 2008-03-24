2925edd04c2a457d5877750d74a8a9c4ae4e5f55
alex <alex@421fdda0-16d4-4d65-83a8-de151944c2bf>
2008-03-24 00:59:58 +0000

    added constants for groups

--- app/models/UserTypeModel.php
+++ app/models/UserTypeModel.php
@@ -1,6 +1,7 @@
 <?php
 class UserTypeModel extends BaseModel{
-	
+	const UNREGISTRED	= 2;
+	const REGISTRED		= 3;
 		function __construct(){
 			parent::__construct('user_type');
 			$this -> _caches(true, true, true);
