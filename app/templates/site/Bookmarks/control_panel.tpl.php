ba0e44ddf691eebbed1e9951b26130a3a860da6f
den <den@421fdda0-16d4-4d65-83a8-de151944c2bf>
2008-05-07 16:55:58 +0000

    Removed file/folder

--- app/templates/site/Bookmarks/control_panel.tpl.php
+++
@@ -1,4 +0,0 @@
-<div class="block_ee1"><div class="block_ee2"><div class="block_ee3"><div class="block_ee4">
-	<div class="block_title"><h2>Управление закладками</h2></div>
-	<p><a href="<?php echo $this->createUrl('Bookmarks', 'BookmarksList');?>">Добавить закладку</a></p>
-</div></div></div></div>
